44abf8658e606ab357b9985c3a9df9114ca44d03
Lorien Chan <waitingchan@Wais-MacBook-Air.local>
2023-12-21 08:41:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import './App.css';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
 
-
 const API_URL = 'http://www.omdbapi.com?apikey=667b1b92';
-
-const movie1 = 
-
-  {
-    "Title": "Taxi Driver",
-    "Year": "1976",
-    "imdbID": "tt0075314",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BM2M1MmVhNDgtNmI0YS00ZDNmLTkyNjctNTJiYTQ2N2NmYzc2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-}
-
-
-
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -38,31 +24,29 @@ const App = () => {
       <h1>Movie Movie</h1>
 
       <div className='search'>
-        <input placeholder="search here"
-        value={terms} onChange={(e) =>setTerms(e.target.value)}></input>
-
-      
+        <input
+          placeholder="search here"
+          value={terms}
+          onChange={(e) => setTerms(e.target.value)}
+        />
         <button className="button" onClick={() => searchMovies(terms)}>
-                            Search
-                </button>
+          Search
+        </button>
       </div>
-        
-      
 
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie} />
+            <MovieCard key={movie.imdbID} movie={movie} />
           ))}
         </div>
       ) : (
-        
         <div className="empty">
-        <h2>No movies found</h2>
-      </div>
-    )}
-  </div>
-);
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
